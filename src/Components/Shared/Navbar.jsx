c368a8b2b354ae9b7848f901a1377a6ae3c61f8f
EgzonBllacaa <egzonbllaca9@gmail.com>
2025-08-09 23:27:48 +0200

    bug fixed

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import Cart from "../../assets/Cart.png";
-import Search from "../../assets/search.png";
+import Search from "../../assets/Search.png";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
